f7cdbf0f1eee35a56940e361d9c155896600d693
Nathalia Uribe <iuribel@unal.edu.co>
2022-11-19 15:49:41 -0500

    wholecommit

---
+++ prueba1.m
@@ -0,0 +1,2 @@
+code1=train('C:\Users\NATHA\Documents\Semestre 2022-2\Teoria de la inormación\Speech-Recognition\data2\train\',5);
+test('C:\Users\NATHA\Documents\Semestre 2022-2\Teoria de la inormación\Speech-Recognition\data2\test\',5,code1);
